7301faf9f25cf18ba660d535d2baea37d36d0085
Albannach <zydeco@copacetic.co.uk>
2015-09-22 00:34:56 +0100

    Second pass, abstracting data operations

--- best.R
+++ best.R
@@ -1,23 +1,13 @@
 best <- function(state, outcome) {
     
-    validOutcomes = c("Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack", 
-                      "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure", 
-                      "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")
-    names(validOutcomes) = c("heart attack", "heart failure", "pneumonia")
+    od = outcomeData()
+    data = od$data()
+
+    od$validateState(state)
+    columnName = od$validateOutcome(outcome)
     
-    ## Read outcome data
-    data = read.csv("rprog_data_ProgAssignment3-data\\outcome-of-care-measures.csv",  
-                           colClasses="character")
-    for (name in validOutcomes)
-        data[, name] = suppressWarnings(as.numeric(data[, name]))
-    
-    ## Check that state and outcome are valid
-    if (!is.element(state, sort(unique(data[, 7])))) stop("invalid state")
-    if (!is.element(outcome, names(validOutcomes))) stop("invalid outcome")
-    
-    ## Return hospital name in that state with lowest 30-day death rate
-    columnName = validOutcomes[match(outcome, names(validOutcomes))]
     columns = c("Hospital.Name", columnName)
-    y = subset(data, data[,"State"] == state & !is.na(data[,columnName]), select=columns)
-    c(y[order(y[,2], y[,1]),][1,1])
+    
+    stateOutcomes = subset(data, data[,"State"] == state & !is.na(data[,columnName]), select=columns)
+    c(stateOutcomes[order(stateOutcomes[,2], stateOutcomes[,1]),][1,1])
 }
---
+++ outcomeData.R
@@ -0,0 +1,24 @@
+outcomeData = function() {
+    
+    validOutcomes = c("Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack", 
+                      "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure", 
+                      "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")
+    names(validOutcomes) = c("heart attack", "heart failure", "pneumonia")
+    
+    ## Read outcome data
+    rawData = read.csv("rprog_data_ProgAssignment3-data\\outcome-of-care-measures.csv",  
+                        colClasses="character")
+    for (name in validOutcomes)
+        rawData[, name] = suppressWarnings(as.numeric(rawData[, name]))
+    
+    states = sort(unique(rawData[, 7]))
+
+    data = function() rawData
+    validateState = function(state) if (!is.element(state, states)) stop("invalid state")
+    validateOutcome = function(outcome) {
+        if (!is.element(outcome, names(validOutcomes))) stop("invalid outcome")
+        validOutcomes[match(outcome, names(validOutcomes))]
+    }
+    
+    list(data=data, validateState=validateState, validateOutcome=validateOutcome)
+}
